bace43ae3a257a35d798f920e21bb224d764e0a7
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-28 19:22:42 -0400

    commit tree e8de8595fa5748a2037dbb2c36035baa07c223e4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, callback) {
+  let r = []
+  for (let index in array) {
+    r.push(callback(array[index]))
+  }
+  return r
+}
+
+function reduce(array, callback, startingPoint) {
+  let r;
+  if (startingPoint) {
+    r = startingPoint
+    for (let index in array) {
+      r = callback(array[index], r)
+    }
+  } else {
+    r = array[0]
+    for (var i = 1; i < array.length; i++) {
+      r = callback(array[i], r)
+    }
+  }
+  return r
+}
